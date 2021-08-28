2f83905e59f50f79e1333d75f6016972c17b9e2f
Mario Frlić <72163524+mfrlic@users.noreply.github.com>
2021-08-28 14:23:59 +0200

    Points

--- data.py
+++ data.py
@@ -46,7 +46,18 @@ def get_incident_data():
         data = requests.get("https://api.sofascore.com/api/v1/event/" + str(match["id"]) + "/incidents").json()["incidents"]
         incidents["incidents"][str(match["id"])] = []
         for incident in data:
-            incidents["incidents"][str(match["id"])].append(incident)
+            if incident["incidentType"] == "period":
+                pass
+            elif incident["incidentType"] == "substitution":
+                incidents["incidents"][str(match["id"])].append({ "playerIn": incident["playerIn"]["id"], "playerOut": incident["playerOut"]["id"], "time": incident["time"], "incidentType": "substitution" })
+            elif incident["incidentType"] == "card":
+                incidents["incidents"][str(match["id"])].append({ "player": incident["player"]["id"], "card": incident["incidentClass"], "time": incident["time"], "incidentType": "card" })
+            elif incident["incidentType"] == "goal":
+                try:
+                    assist = incident["assist1"]["id"]
+                except:
+                    assist = "N/A"
+                incidents["incidents"][str(match["id"])].append({ "goalscorer": incident["player"]["id"], "assist": assist, "time": incident["time"], "incidentType": "goal" })
 
     with open("incidents.json", "w") as file:
         json.dump(incidents, file, indent=4)
@@ -80,14 +91,83 @@ def get_player_data():
     with open("players.json", "w") as file:
         json.dump(players, file, indent=4)
 
-def get_points_data():
-    pass
+def get_lineup_data():
+    lineups = {"lineups": {}}
+
+    with open("matches.json") as file:
+        matches = json.load(file)
+
+    for match in matches["matches"]:
+        data = requests.get("https://api.sofascore.com/api/v1/event/" + str(match["id"]) + "/lineups").json()
+        lineups["lineups"][str(match["id"])] = { "home": [], "away": [] }
+        for player in data["home"]["players"]:
+
+            try:
+                minutesPlayed = player["statistics"]["minutesPlayed"]
+            except:
+                minutesPlayed = 0
+
+            if minutesPlayed != 0:
+                if player["position"] == "G":
+                    try:
+                        saves = player["statistics"]["saves"]
+                    except:
+                        saves = 0
+                    lineups["lineups"][str(match["id"])]["home"].append({ "id": player["player"]["id"], 
+                                                                        "minutesPlayed": minutesPlayed, 
+                                                                        "saves": saves, 
+                                                                        "substitute": player["substitute"] })
+                else: 
+                    lineups["lineups"][str(match["id"])]["home"].append({ "id": player["player"]["id"], 
+                                                                        "minutesPlayed": minutesPlayed, 
+                                                                        "substitute": player["substitute"] })
+
+        for player in data["away"]["players"]:
+
+            try:
+                minutesPlayed = player["statistics"]["minutesPlayed"]
+            except:
+                minutesPlayed = 0
+
+            if minutesPlayed != 0:
+                if player["position"] == "G":
+                    try:
+                        saves = player["statistics"]["saves"]
+                    except:
+                        saves = 0
+                    lineups["lineups"][str(match["id"])]["away"].append({ "id": player["player"]["id"], 
+                                                                        "minutesPlayed": (player["statistics"]["minutesPlayed"] if player["statistics"] != {} else 0), 
+                                                                        "saves": saves, 
+                                                                        "substitute": player["substitute"] })
+                else: 
+                    lineups["lineups"][str(match["id"])]["away"].append({ "id": player["player"]["id"], 
+                                                                        "minutesPlayed": minutesPlayed, 
+                                                                        "substitute": player["substitute"] })
+
+    with open("lineups.json", "w") as file:
+        json.dump(lineups, file, indent=4)
+
+def get_point_data():
+    points = {"points": {}}
+
+    with open("incidents.json") as file:
+        incidents = json.load(file)
+
+    with open("lineups.json") as file:
+        lineups = json.load(file)
+
+
 
 def get_transfer_data():
     pass
 
+#get lineups so you can count points
+#find a way to fetch transfers and disable players not playing anymore
 
 # get_teams_data()
 # get_match_data()
-# get_incident_data()
-get_player_data()
+get_incident_data()
+# get_player_data()
+# get_lineup_data()
+# get_point_data()
+# get_transfer_data()
